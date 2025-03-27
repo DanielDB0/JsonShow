46e6014e79993891465fcb3a224d891e4e937d89
3DS <3ds@bglab.local>
2025-03-27 10:40:34 -0300

    Second Commit

--- lib/filmes.dart
+++ lib/filmes.dart
@@ -1,26 +1,42 @@
 class Filmes {
   late String Nome;
   late DateTime Data;
-  late String diretor;
+  late String Diretor;
   late int Tempo;
   late double Nota;
 
   Filmes(){
     Nome = "";
     Data = DateTime.now();
-    diretor = "";
+    Diretor = "";
     Tempo = 0;
     Nota = 0;
   }
 
-  Filmes.v(this.Nome, this.Data, this.Nota, this.Tempo, this.diretor);
+  Filmes.v(this.Nome, this.Data, this.Nota, this.Tempo, this.Diretor);
 
   Filmes.fromJson(Map<String, dynamic> json) :
   Nome = json['Nome'] as String,
-  Data = json['Data'] as DateTime,
-  diretor = json['Diretor'] as String,
+  Data = Filmes.tratarData(json['Data']),
+  Diretor = json['Diretor'] as String,
   Tempo = json['Tempo'] as int,
   Nota = json['Nota'] as double;
 
-  
+  Map<String, dynamic> toJson() => {
+        'Nome': Nome,
+        'Data': Data,
+        'Diretor': Diretor,
+        'Tempo': Tempo,
+        'Nota': Nota,
+      };
+
+  static DateTime tratarData(String dt)
+  {
+    final d = dt.toString().split('/');
+
+    final ano = d[0].isEmpty?2000:d[0];
+    final mes = d[1].isEmpty?01:d[1];
+    final dia = d[2].isEmpty?01:d[2];
+    return DateTime(int.parse(ano.toString()),int.parse(mes.toString()), int.parse(dia.toString()));
+}
 }     
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:json_show/filmes.dart';
+import 'package:'
 
 void main() {
-  runApp(const MainApp());
+  runApp( const MaterialApp (title: "App",
+      home: MainApp(),));
 }
 
-class MainApp extends StatelessWidget {
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+
   @override
+  Exibir createState() => Exibir ();
+}
+
+const List<Filmes> Filme = Filmes.fromJson();
+
+class Exibir extends State<MainApp> { 
+
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          
+        body: ListView.builder(
+          itemCount: Filme.length,
+         shrinkWrap: true,
+          itemBuilder: (BuildContext ctx, index){
+            return Filme.Nome[index];
+          }
         ),
       ),
     );
